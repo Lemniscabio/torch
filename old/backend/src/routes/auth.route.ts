bcca000200ccd9d5392bcb145187085ca5ab5486
kartikeyW1 <kartikey.attri@lemnisca.bio>
2026-05-18 10:47:52 +0530

    app restructure + UI/SERVER separated

--- old/backend/src/routes/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleAuth } from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post("/", handleAuth);
-
-export default router;
--- old/backend/src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUser } from "../controllers/user.controller";
-import { requireAuth } from "../middlewares/auth.middleware";
-
-const router = Router();
-
-router.get("/", requireAuth, getUser);
-
-export default router;
